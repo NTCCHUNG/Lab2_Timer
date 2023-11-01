fe65a5e49b03b239c2f674eb93021e499768f54f
DESKTOP-LVG3KK4\DELL <chung.nguyenchung@hcmut.edu.vn>
2023-11-01 21:57:16 +0700

    Init Lab2

---
+++ STM/Lab2_Ex/Core/Inc/sortware_timer.h
@@ -0,0 +1,27 @@
+/*
+ * sortware_timer.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: DELL
+ */
+
+#ifndef INC_SORTWARE_TIMER_H_
+#define INC_SORTWARE_TIMER_H_
+
+extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
+extern int timer4_flag;
+extern int timer5_flag;
+extern int timer6_flag;
+
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void setTimer4(int duration);
+void setTimer5(int duration);
+void setTimer6(int duration);
+
+void timerRun();
+
+#endif /* INC_SORTWARE_TIMER_H_ */
